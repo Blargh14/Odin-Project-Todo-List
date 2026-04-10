eb438b7571b133f811b2e51ce63b663414854409
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2026-04-10 01:33:54 -0400

    Give todo item readability

--- src/display.js
+++ src/display.js
@@ -12,10 +12,33 @@ export function populateContent(project) {
 
     project.todoList.forEach((todo) => {
         const todoDiv = document.createElement("div");
-
         todoDiv.classList.add("todoitem");
 
-        todoDiv.textContent = todo.title + todo.description + todo.dueDate + todo.priority + todo.checklist;
+        const titleP = document.createElement("p");
+        titleP.classList.add("title");
+        titleP.textContent = todo.title;
+        todoDiv.appendChild(titleP);
+
+        const descP = document.createElement("p");
+        descP.classList.add("description");
+        descP.textContent = todo.description;
+        todoDiv.appendChild(descP);
+
+        const infoBar = document.createElement("div");
+        infoBar.classList.add("infobar");
+        todoDiv.appendChild(infoBar);
+
+        const dateP = document.createElement("p");
+        dateP.classList.add("duedate");
+        dateP.textContent = "Due: " + todo.dueDate;
+        infoBar.appendChild(dateP);
+
+        const priorityP = document.createElement("p");
+        priorityP.classList.add("duedate");
+        priorityP.textContent = "Importance: " + todo.priority;
+        infoBar.appendChild(priorityP);
+
+        // todoDiv.textContent = todo.title + todo.description + todo.dueDate + todo.priority + todo.checklist;
 
         content.appendChild(todoDiv);
     });
